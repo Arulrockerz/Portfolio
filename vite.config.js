833dd98897bc336ff846062048254cdbc60830ec
Arulsiddharthan s <sarulsiddharthan10@gmail.com>
2025-08-23 10:59:42 +0530

    Allow Render host for Vite preview

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    host: true,
+    port: 4173, // optional, or use $PORT in Render Start Command
+    allowedHosts: ['portfolio-6-t3xf.onrender.com']
+  }
+});
