bc3c2b7e4c0b52f5eeffbefb4f66d486e2be2621
Arulsiddharthan s <sarulsiddharthan10@gmail.com>
2025-08-27 18:59:21 +0530

    Updated Contact.jsx to send messages to arulsiddhu91@gmail.com via EmailJS

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -36,9 +36,7 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "Portfolio",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
@@ -46,7 +44,7 @@ const Contact = () => {
       .then(
         () => {
           setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
+          alert("✅ Thank you. I will get back to you as soon as possible.");
 
           setForm({
             name: "",
@@ -58,7 +56,7 @@ const Contact = () => {
           setLoading(false);
           console.error(error);
 
-          alert("Ahh, something went wrong. Please try again.");
+          alert("❌ Ahh, something went wrong. Please try again.");
         }
       );
   };
